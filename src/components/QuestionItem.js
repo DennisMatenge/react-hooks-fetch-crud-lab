dd8eab18983ce5f135eee14c2a13c6a3f5b16c71
DennisMatenge <mateshdennis@gmail.com>
2023-05-20 20:01:15 +0300

    first commit

---
+++ src/components/QuestionItem.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function QuestionItem({ question }) {
+  const { id, prompt, answers, correctIndex } = question;
+
+  const options = answers.map((answer, index) => (
+    <option key={index} value={index}>
+      {answer}
+    </option>
+  ));
+
+  function handleDelete(e) {
+    fetch(`http://localhost:4000/questions/:id`,{
+      method: " DELETE",
+      body: ""
+    })
+  } 
+
+  return (
+    <li>
+      <h4>Question {id}</h4>
+      <h5>Prompt: {prompt}</h5>
+      <label>
+        Correct Answer:
+        <select defaultValue={correctIndex}>{options}</select>
+      </label>
+      <button onClick={handleDelete}>Delete Question</button>
+    </li>
+  );
+}
+
+export default QuestionItem;
---
+++ src/components/QuestionList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import QuestionItem from "./QuestionItem";
+
+function QuestionList({questions}) {
+  return (
+    <section>
+      <h1>Quiz Questions</h1>
+      <ul>{questions.map(question => (
+        <QuestionItem key = {question.id} question={question} />
+
+      ))}</ul>
+    </section>
+  );
+}
+
+export default QuestionList;
